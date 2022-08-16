11fb29af96b64acc4e80535245d4b3fdedd6feca
Ahmed Ali <72547276+Ahmed130405@users.noreply.github.com>
2022-08-16 17:04:57 +0300

    Added code to download data

--- e2Dist/e2Dist.py
+++ e2Dist/e2Dist.py
@@ -12,7 +12,25 @@ rcParams['font.family'] = 'sans-serif'
 rcParams['font.sans-serif'] = ['Helvetica']
 rcParams['pdf.fonttype'] = 42
 
-dataString = "./Dropbox/main_1/data/trento_Pb_Pb_4000000_-x_6.4_-n_13.94_-k_1.19_-p_0.007_-d_0.7_-w_0.4.csv"
+import os
+from os import path
+import urllib.request
+
+_DATA = "/tmp/e2Dist_data/"
+
+def _download(url, filename):
+  if not path.exists(_DATA):
+    os.makedirs(_DATA)
+  out_file = path.join(_DATA, filename)
+  if not path.isfile(out_file):
+    urllib.request.urlretrieve(url, out_file)
+
+url = "https://www.dropbox.com/s/8u3r2ny9o5vwvh1/trento_Pb_Pb_4000000_-x_6.4_-n_13.94_-k_1.19_-p_0.007_-d_0.7_-w_0.4.csv?dl=0"
+filename = "trento_Pb_Pb_4000000_-x_6.4_-n_13.94_-k_1.19_-p_0.007_-d_0.7_-w_0.4.csv"
+
+_download(url,filename)
+
+dataString = path.join(_DATA,filename)
 data = pd.read_csv(dataString)
 nch = np.asarray(data.iloc[:,3])
 e2 = np.asarray(data.iloc[:,4])
@@ -26,7 +44,7 @@ fig, ax = plt.subplots()
 ax.plot(x,y,color='blue',linestyle='dashed',linewidth=2)
 ax.set_xlabel('Normalized e2')
 ax.set_title('Distribution for 0 to 5% centrality')
-figString = "./Dropbox/main_1/results/figure1.png"
+figString = path.join(_DATA,"figure1.png")
 plt.savefig(figString,format='png')
 plt.show()
 
